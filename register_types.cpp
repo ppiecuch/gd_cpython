a82f918a40ed1f4d4ee60c00a8837c94411b49e0
Pawel Piecuch <pawel.piecuch@roche.com>
2021-06-12 17:07:21 +0200

    CPython and Godot module configuration

---
+++ register_types.cpp
@@ -0,0 +1,8 @@
+#include "core/class_db.h"
+#include "register_types.h"
+
+void register_gd_cpython_types() {
+}
+
+void unregister_gd_cpython_types() {
+}
---
+++ register_types.h
@@ -0,0 +1,7 @@
+#ifndef GD_CPYTHON_REGISTER_TYPES_H
+#define GD_CPYTHON_REGISTER_TYPES_H
+
+void register_gd_cpython_types();
+void unregister_gd_cpython_types();
+
+#endif // GD_CPYTHON_REGISTER_TYPES_H
